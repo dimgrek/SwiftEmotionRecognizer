89c1318c9cd2991a798bb1180c739abc4a6781a9
Dmytro Hrek <dhr@mb-218023.kyiv.ciklum.net>
2016-11-30 11:19:15 +0200

    Initial Commit

---
+++ EmotionRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmotionRecognizer
+//
+//  Created by Dmytro Hrek on 11/30/16.
+//  Copyright © 2016 Dmytro Hrek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
