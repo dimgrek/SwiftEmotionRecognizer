4c98ae7ed024bd12077f816a0e66d7e481dc352d
John Nash <dimgrek@gmail.com>
2016-12-08 16:29:51 +0200

    UI added, async loading of images added

--- EmotionRecognizer/ViewController.swift
+++ EmotionRecognizer/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    let subscriptionKey = "40778dfbd2884ee4870526cf17cc15bd"
+    
+    
+    let Emotions = [
+        "Anger": "http://www.lindaclarke.co.za/wp-content/uploads/2012/11/Image-of-Anger.jpg",
+        "Surprise": "http://psychology.iresearchnet.com/wp-content/uploads/2016/01/Surprise.jpg",
+        "Happiness" : "https://s-media-cache-ak0.pinimg.com/736x/08/15/db/0815db06df850e27e74411a3232ffa3e.jpg",
+        "Disgust": "https://ocdinformation.files.wordpress.com/2010/09/disgust.jpg"    ]
+    
+    
+    func EmotionUrlNamed(imageN: String?) -> NSURL?{
+        if let urlstring =  Emotions[imageN ?? ""]{
+            return NSURL(string: urlstring)
+        }else{
+            return nil
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var imageUrl: NSURL? {
+        didSet{
+            image = nil
+            fetchImage()
+        }
     }
-
-
+    
+    private var image: UIImage? {
+        get{
+            return imageView.image
+        }
+        set{
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageUrl{
+            // Move to a background thread to do some long running work
+            DispatchQueue.global().async {
+                let contentsOf = NSData(contentsOf: url as URL)
+                // Bounce back to the main thread to update the UI
+                DispatchQueue.main.async {
+                    if url == self.imageUrl{
+                        if let imageData = contentsOf{
+                            self.image = UIImage(data: imageData as Data)
+                        }else{
+                            print("ignored data returned from url \(url)")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func findEmotion(_ sender: UIButton) {
+        imageUrl = EmotionUrlNamed(imageN: sender.currentTitle)
+    }
+    
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
 }
 
